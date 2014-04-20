035d106685d84f64098d538f61b8d1d6a0c97f7c
lealbaugh <l.e.albaugh@gmail.com>
2014-04-19 21:45:02 -0400

    Testing texting

--- hello.py
+++ hello.py
@@ -1,8 +1,46 @@
 from flask import *
+import twilio.twiml
+from twilio.rest import TwilioRestClient
+import os 
+from pymongo import *
+import datetime
+import random
+import re
 
 debug = False
 app = Flask(__name__)
 
+
+# Twilio account info, to be gotten from Heroku environment variables
+account_sid = os.environ['ACCOUNT_SID'] 
+auth_token = os.environ['AUTH_TOKEN']
+twilionumber = os.environ['TWILIO']
+mynumber = os.environ['ME']
+# Init twilio
+twilioclient = TwilioRestClient(account_sid, auth_token)
+
+# MongoHQ account info, also from Heroku environment variables
+mongoclientURL = os.environ['MONGOHQ_URL']
+databasename = mongoclientURL.split("/")[-1] #gets the last bit of the URL, which is the database name
+# Init Mongo
+mongoclient = MongoClient(mongoclientURL)
+database = mongoclient[databasename]	#loads the assigned database
+# collection = database["phonenumber"] #loads or makes the collection, whichever should happen
+players = database["players"]
+transcript = database["transcript"]
+games = database["games"]
+
+
+
 @app.route('/', methods=['GET'])
 def greet():
 	return "hello"
+
+
+@app.route('send', methods=['GET'])
+def sendToRecipient():
+	try:
+		message = twilioclient.sms.messages.create(body="hello", to=mynumber, from_=twilionumber)
+ 	except twilio.TwilioRestException as e:
+ 		print "twilio error: "+e
+ 	return "sent message"
